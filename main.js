bb0c0a1df8cce9c81c1dcf80eabe64eb624cbe3c
delroscol98 <collindelrosario98@gmail.com>
2024-04-11 21:03:33 +1000

    CSS and JS file modified

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const shareBtns = document.querySelectorAll(".share-button");
 
 const toggleShareHandler = () => {
-  const authorSection = document.querySelector(".author");
   const shareSection = document.querySelector(".share");
 
-  authorSection.classList.toggle("hidden");
   shareSection.classList.toggle("hidden");
 };
 
